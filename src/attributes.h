0c0f538c8fe0588fb492135294b1e0186e0fce23
Marco Leogrande <dark.knight.ita@gmail.com>
2019-03-01 21:56:00 -0800

    Use namespaces instead

--- src/attributes.h
+++ src/attributes.h
@@ -28,73 +28,72 @@
 #define NOTE_KEY_ADDRESS_STATE "addr_state"
 #define NOTE_KEY_LOAN_ISSUE_DATE "Loan Issue Date"
 
-class Attributes {
-public:
-  static inline int parse_term(QString s) {
-    return s.toInt();
-  }
+namespace Attributes {
+inline int parse_term(QString s) {
+  return s.toInt();
+}
 
-  static inline QString term_to_string(int term) {
-    return QString::number(term);
-  }
+inline QString term_to_string(int term) {
+  return QString::number(term);
+}
 
-  enum class NoteStatus {
-    UNKNOWN = 0,
-    CHARGED_OFF,
-    CURRENT,
-    DEFAULT,
-    FULLY_PAID,
-    GRACE_PERIOD,
-    ISSUED,
-    LATE_16_30,
-    LATE_31_120
-  };
+enum class NoteStatus {
+  UNKNOWN = 0,
+  CHARGED_OFF,
+  CURRENT,
+  DEFAULT,
+  FULLY_PAID,
+  GRACE_PERIOD,
+  ISSUED,
+  LATE_16_30,
+  LATE_31_120
+};
 
-  static inline NoteStatus parse_status(QString s) {
-    if (s == "Charged Off")
-      return NoteStatus::CHARGED_OFF;
-    else if (s == "Current")
-      return NoteStatus::CURRENT;
-    else if (s == "Default")
-      return NoteStatus::DEFAULT;
-    else if (s == "Fully Paid")
-      return NoteStatus::FULLY_PAID;
-    else if (s == "In Grace Period")
-      return NoteStatus::GRACE_PERIOD;
-    else if (s == "Issued")
-      return NoteStatus::ISSUED;
-    else if (s == "Late (16-30 days)")
-      return NoteStatus::LATE_16_30;
-    else if (s == "Late (31-120 days)")
-      return NoteStatus::LATE_31_120;
-    return NoteStatus::UNKNOWN;
-  }
+inline NoteStatus parse_status(QString s) {
+  if (s == "Charged Off")
+    return NoteStatus::CHARGED_OFF;
+  else if (s == "Current")
+    return NoteStatus::CURRENT;
+  else if (s == "Default")
+    return NoteStatus::DEFAULT;
+  else if (s == "Fully Paid")
+    return NoteStatus::FULLY_PAID;
+  else if (s == "In Grace Period")
+    return NoteStatus::GRACE_PERIOD;
+  else if (s == "Issued")
+    return NoteStatus::ISSUED;
+  else if (s == "Late (16-30 days)")
+    return NoteStatus::LATE_16_30;
+  else if (s == "Late (31-120 days)")
+    return NoteStatus::LATE_31_120;
+  return NoteStatus::UNKNOWN;
+}
 
-  static inline QString status_to_string(NoteStatus status) {
-    switch (status) {
-      case NoteStatus::UNKNOWN:
-        return "<unknown>";
-      case NoteStatus::CHARGED_OFF:
-        return "Charged Off";
-      case NoteStatus::CURRENT:
-        return "Current";
-      case NoteStatus::DEFAULT:
-        return "Default";
-      case NoteStatus::FULLY_PAID:
-        return "Fully Paid";
-      case NoteStatus::GRACE_PERIOD:
-        return "In Grace Period";
-      case NoteStatus::ISSUED:
-        return "Issued";
-      case NoteStatus::LATE_16_30:
-        return "Late (16-30 days)";
-      case NoteStatus::LATE_31_120:
-        return "Late (31-120 days)";
+inline QString status_to_string(NoteStatus status) {
+  switch (status) {
+    case NoteStatus::UNKNOWN:
+      return "<unknown>";
+    case NoteStatus::CHARGED_OFF:
+      return "Charged Off";
+    case NoteStatus::CURRENT:
+      return "Current";
+    case NoteStatus::DEFAULT:
+      return "Default";
+    case NoteStatus::FULLY_PAID:
+      return "Fully Paid";
+    case NoteStatus::GRACE_PERIOD:
+      return "In Grace Period";
+    case NoteStatus::ISSUED:
+      return "Issued";
+    case NoteStatus::LATE_16_30:
+      return "Late (16-30 days)";
+    case NoteStatus::LATE_31_120:
+      return "Late (31-120 days)";
     }
-    return "<error>";
-  }
+  return "<error>";
+}
 
-  static inline QDate parse_loan_issue_date(QString s) {
-    return QDate::fromString(s, "MM/dd/yyyy");
-  }
+inline QDate parse_loan_issue_date(QString s) {
+  return QDate::fromString(s, "MM/dd/yyyy");
+}
 };
